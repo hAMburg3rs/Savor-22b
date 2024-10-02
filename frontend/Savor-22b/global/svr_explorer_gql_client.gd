3e9289201cc8c98b03af65cdbd14a84b8e00ddfe
Atralupus <me@atralupus.com>
2024-10-02 22:39:57 +0900

    Remove old frontend

--- frontend/Savor-22b/global/svr_explorer_gql_client.gd
+++
@@ -1,9 +0,0 @@
-extends GQLClient
-
-func _ready():
-		
-	set_endpoint(false, "localhost", 38080, "/graphql/explorer")
-	
-
-func raw_mutation(query:String):
-	return GQLQueryExecuter.new(endpoint, use_ssl, Mutation.new(query))
--- frontend/Savor-22b/global/svr_gql_client.gd
+++
@@ -1,9 +0,0 @@
-extends GQLClient
-
-func _ready():
-	
-	set_endpoint(false, "localhost", 38080, "/graphql")
-	
-
-func raw_mutation(query:String):
-	return GQLQueryExecuter.new(endpoint, use_ssl, Mutation.new(query))
